712eb855e7406a577acdebc499dfee6e8aafd5d1
Tygo van Dorst <D295661@edu.curio.nl>
2024-09-04 10:03:37 +0200

    added index en start function

--- app/Http/Controllers/SceneController.php
+++ app/Http/Controllers/SceneController.php
@@ -4,7 +4,20 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class SceneController extends Controller
+class GameController extends Controller
 {
-    //
+    // Display the start page
+    public function index()
+    {
+        return view('game.index');
+    }
+
+    // Handle game start logic
+    public function start(Request $request)
+    {
+        // Logic to start the game (e.g., initializing player session, loading data, etc.)
+        
+        // For now, just redirect to a game page or send a response
+        return redirect()->route('game.index')->with('status', 'Het avontuur is begonnen!');
+    }
 }
