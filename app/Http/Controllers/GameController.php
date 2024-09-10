35a1c390bf009b68d2ef5aa4d67dd24bca96adda
Tygo van Dorst <D295661@edu.curio.nl>
2024-09-10 15:20:55 +0200

    Added GameController

---
+++ app/Http/Controllers/GameController.php
@@ -0,0 +1,25 @@
+<?php
+
+// app/Http/Controllers/GameController.php
+namespace App\Http\Controllers;
+
+use App\Models\Scene;
+use App\Models\Hint;
+use Illuminate\Http\Request;
+
+class GameController extends Controller
+{
+    public function showScene($id)
+    {
+        $scene = Scene::with('choices')->findOrFail($id);
+        return view('game.index', compact('scene')); // Change 'adventure.index' to 'game.index'
+    }
+
+
+    public function showHint($scene_id)
+    {
+        $hint = Hint::where('scene_id', $scene_id)->first();
+        return response()->json(['hint' => $hint ? $hint->hint : 'No hint available for this scene.']);
+    }
+}
+
